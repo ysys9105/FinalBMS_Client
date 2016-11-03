ae885e4c6b41e4a91ae24ed18036e283a7221769
ysys9105@gmail.com <fkqns9191>
2016-11-03 09:40:30 +0900

    목요일, 도서관리시스템 도서검색/수정,상제,입력(이미지포함) 구현완료

--- js/index.js
+++ js/index.js
@@ -6,9 +6,13 @@ var titleTd = null;
 var authorTd = null;
 var img = null;
 var imgTd = null;
+var aboutTd = null;
 var tr = null;
 var dbtn = null;
 var ubtn = null;
+var abtn = null;
+var base64 = null;
+
 
 function searchBook() {
     if(event.keyCode==13){
@@ -22,43 +26,137 @@ function searchBook() {
             },
 
             success : function(result){
+                $("tbody").empty();
                 //결과창출력
-
 
                 for (var i = 0; i < result.length; i++) {
                     tr = $("<tr></tr>").attr("data-isbn",result[i].isbn);
 
                     priceTd = $("<td></td>").text(result[i].price);
-                    titleTd = $("<td></td>").text(result[i].title);
+                    titleTd = $("<td></td>").text(result[i].title).attr("id","tTd"+result[i].isbn);
                     authorTd = $("<td></td>").text(result[i].author);
                     img = $("<img />").attr("src",result[i].img);
                     imgTd = $("<td></td>").append(img);
 
-
                     dbtn =$("<input>");
                     dbtn.attr("type","button");
+                    dbtn.attr("class","btn btn-danger");
                     dbtn.attr("value","삭제");
+
+                    //DELETE 기능!! *********************************
                     dbtn.on("click",function(){
-                        $(this).parent().parent().remove();
+                            //update처리!!
+                            //DB처리, 화면처리
+                            //1.AJAX 호출해 서버프로그램을 실행시켜 DB처리
+                        var isbn = $(this).parent().parent().attr("data-isbn");
+                        var pos =  $(this).parent().parent();
+                            $.ajax({
+                                url : "http://localhost:7070/book/bookDelete",
+                                type : "get",
+                                dataType : "jsonp",
+                                jsonp : "callback",
+                                data : {
+                                    isbn : isbn
+
+                                },
+                                success : function(){
+
+                                    pos.remove();
+                                    alert("삭제되었습니다!")
+                                },
+                                error : function () {
+                                    alert("삭제 에러 발생!!")
+                                }
+                            });
+
+                    });
+                    var dbtnTd = $("<td></td>").append(dbtn);
+
+                    abtn =$("<input>");
+                    abtn.attr("type","button");
+                    abtn.attr("class","btn btn-info");
+                    abtn.attr("value","상세보기");
+
+                    aboutTd = $("<td></td>").append(abtn);
+
+                    //상세보기 기능!! *********************************
+                    abtn.on("click",function(){
+                        //상세정보 처리!!
+                        //DB처리, 화면처리
+                        //1.AJAX 호출해 서버프로그램을 실행시켜 DB처리
+                        var isbn = $(this).parent().parent().attr("data-isbn");
+                        console.log(isbn);
+                        $.ajax({
+                            url : "http://localhost:7070/book/bookAbout",
+                            type : "get",
+                            dataType : "jsonp",
+                            jsonp : "callback",
+                            data : {
+                                isbn : isbn
+
+                            },
+                            success : function(result){
+
+                                //*****************상세보기기능*****************************
+                                var date = result.date;
+                                var page = result.page;
+                                var translator = result.translator;
+                                var supple = result.supplement;
+                                var detail = "<b>도서정보 :</b><br>"+"ISBN: "+ isbn + "<br>"+"Date: " + date +"<br>"+ "Page: "+ page+"<br>"+"Translator: "+ translator +"<br>"+ "Supplement: "+supple;
+
+                                var d1 = $("<div></div>").attr("id","detail").append(detail);
+                                $("#tTd"+isbn).append(d1);
+
+                            },
+                            error : function () {
+                                alert("불러오기 에러 발생!!")
+                            }
+                        });
+
                     });
 
-                    var dbtnTd = $("<td></td>").append(dbtn);
+
+
 
                     ubtn = $("<input>");
                     ubtn.attr("type","button");
                     ubtn.attr("value","수정");
+                    ubtn.attr("class","btn btn-primary");
+                    ubtn.attr("id","update");
+
+                    //UPDATE 기능!! *********************************
                     ubtn.on("click",function(){
                         //수정처리
+                        var title = $(this).parent().parent().find("td:nth-child(2)").text();
+                        var author = $(this).parent().parent().find("td:nth-child(3)").text();
                         var price = $(this).parent().parent().find("td:nth-child(4)").text();
-                        var updatebox=$("<input />").attr("type","text").val(price);
-                        updatebox.on("keyup",function () {
-                            if(event.keyCode==13){
+                        var updatetitle=$("<input />").attr("type","text").val(title);
+                        var updateauthor=$("<input />").attr("type","text").val(author);
+                        var updateprice=$("<input />").attr("type","text").val(price);
+
+
+                        $(this).parent().parent().find("td:nth-child(2)").text("");
+                        $(this).parent().parent().find("td:nth-child(2)").append(updatetitle);
+                        $(this).parent().parent().find("td:nth-child(3)").text("");
+                        $(this).parent().parent().find("td:nth-child(3)").append(updateauthor);
+                        $(this).parent().parent().find("td:nth-child(4)").text("");
+                        $(this).parent().parent().find("td:nth-child(4)").append(updateprice);
+                        $(this).parent().parent().find("[value=삭제]").attr("disabled","disabled");
+                        $(this).parent().parent().find("[value=상세보기]").attr("disabled","disabled");
+                        $(this).parent().parent().find("#update").attr("value","완료");
+                        var update =$(this).parent().parent().find("#update");
+
+                        update.on("click",function () {
                                 //update처리!!
                                 //DB처리, 화면처리
                                 //1.AJAX 호출해 서버프로그램을 실행시켜 DB처리
 
                                 var isbn = $(this).parent().parent().attr("data-isbn");
-                                var price = $(this).val();
+
+                                var title1 = updatetitle.val();
+                                var author1 = updateauthor.val();
+                                var price1 = updateprice.val();
+
                                 var tr = $(this).parent().parent();
                                 $.ajax({
                                     url : "http://localhost:7070/book/bookUpdate",
@@ -67,33 +165,39 @@ function searchBook() {
                                     jsonp : "callback",
                                     data : {
                                         isbn : isbn,
-                                        price : price
+                                        title : title1,
+                                        author : author1,
+                                        price : price1
                                     },
                                     success : function(result){
-                                        alert("정상입니다");
+                                        alert("변경되었습니다");
+                                        tr.find("td:nth-child(2)").empty();
+                                        tr.find("td:nth-child(2)").text(title1);
+                                        tr.find("td:nth-child(3)").empty();
+                                        tr.find("td:nth-child(3)").text(author1);
                                         tr.find("td:nth-child(4)").empty();
-                                        tr.find("td:nth-child(4)").text(price);
+                                        tr.find("td:nth-child(4)").text(price1);
                                     },
                                     error : function () {
                                         alert("업데이트 에러 발생!!")
                                     }
                                 });
 
-                            }
-                        })
-                        $(this).parent().parent().find("td:nth-child(4)").text("");
-                        $(this).parent().parent().find("td:nth-child(4)").append(updatebox);
-                        $(this).parent().parent().find("[type=button]").attr("disabled","disabled");
-                    });
 
+                        })
+                    });
                     var ubtnTd = $("<td></td>").append(ubtn);
+
+
+
 
                     tr.append(imgTd);
                     tr.append(titleTd);
                     tr.append(authorTd);
                     tr.append(priceTd);
                     tr.append(dbtnTd);
-                    tr.append(ubtnTd)
+                    tr.append(ubtnTd);
+                    tr.append(aboutTd);
 
                     $("tbody").append(tr);
                 }
@@ -106,8 +210,70 @@ function searchBook() {
     }
 }
 
+function insertBook() {
+
+    var isbn = $("#isbni").val();
+    var title = $("#titlei").val();
+    var date = $("#datei").val();
+    var page = $("#pagei").val();
+    var price = $("#pricei").val();
+    var author = $("#authori").val();
+    var translator = $("#translatori").val();
+    var supplement = $("#supplementi").val();
+    var publisher = $("#publisheri").val();
+    var imgurl = $("#imgurli").val();
 
 
-function mySort() {
 
+    console.log("..." + isbn);
+    console.log("...2 " + title);
+
+    $.ajax({
+        url : "http://localhost:7070/book/bookInsert",
+        type : "get",
+        dataType : "jsonp",
+        jsonp : "callback",
+        data : {
+            isbn : isbn,
+            title : title,
+            date : date,
+            page : page,
+            price : price,
+            author : author,
+            translator : translator,
+            supplement : supplement,
+            publisher : publisher,
+            imgurl : imgurl,
+            imgbase64 : base64
+        },
+        success : function(result){
+            alert("변경되었습니다");
+            $(location).attr("href","index.html")
+
+        },
+        error : function () {
+            alert("업데이트 에러 발생!!")
+        }
+    });
+
+}
+
+function fileInfo(f){
+    var file = f.files; // files 를 사용하면 파일의 정보를 알 수 있음
+
+    if(file[0].size > 1024 * 1024){
+        // 큰 파일을 올리니까 브라우저가 다운되었음 -_-;;
+        alert('1MB 이하 이미지를 업로드 해주세요');
+        return;
+    }
+
+    var reader = new FileReader();
+
+    reader.onload = function(rst){
+        $('#imgbase64i').html('<img src="' + rst.target.result + '">');
+
+        base64=rst.target.result;
+
+    }
+    reader.readAsDataURL(file[0]); // 파일을 읽는다, 배열이기 때문에 0 으로 접근
 }
